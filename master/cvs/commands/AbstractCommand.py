35419ef5f79aa920649b8179887713f6a0739b06
Tyoma Gorbunov <bivaetctoskazat123@gmail.com>
2025-05-18 17:51:12 +0500

    refactor: Получение путей до директорий вынесено в AbstractCommand

--- master/cvs/commands/AbstractCommand.py
+++ master/cvs/commands/AbstractCommand.py
@@ -12,3 +12,9 @@ class AbstractCommand(ABC):
         if not os.path.isdir(os.path.join(os.getcwd(), ".cvs")):
             print("\033[91mError: CVS repository not initialized in this directory.\033[0m")
             exit(1)
+
+    @classmethod
+    def _get_dirs_paths(cls):
+        current_dir = os.getcwd()
+        cvs_dir = os.path.join(current_dir, ".cvs")
+        return current_dir, cvs_dir
--- master/cvs/commands/InitCommand.py
+++ master/cvs/commands/InitCommand.py
@@ -10,23 +10,25 @@ class InitCommand(AbstractCommand):
     def __init__(self):
         self.name = Init.name
         self.description = Init.description
+        self.__dir, self.__cvs_dir = self._get_dirs_paths()
 
     def run(self, args):
         self.__init_folder()
         self.__init_cvs_insides()
 
     def __init_folder(self):
-        path = os.getcwd()
-        cvs_dir = os.path.join(path, ".cvs")
-        self.__cvs_dir = cvs_dir
-        if os.path.exists(cvs_dir):
+        if os.path.exists(self.__cvs_dir):
             print("\033[93mRepository is already initialized\033[0m")
             exit(1)
-        os.makedirs(cvs_dir)
+        os.makedirs(self.__cvs_dir)
         if os.name == 'nt':
             attr_hidden = 0x02
-            ctypes.windll.kernel32.SetFileAttributesW(cvs_dir, attr_hidden)
-        print(f"\033[92mInitialized empty CVS repository in {path}\033[0m")
+            ctypes.windll.kernel32.SetFileAttributesW(
+                self.__cvs_dir,
+                attr_hidden
+            )
+        print(
+            f"\033[92mInitialized empty CVS repository in {self.__dir}\033[0m")
 
     def __init_cvs_insides(self):
         objects_dir = os.path.join(self.__cvs_dir, "objects")
