66a9e3172f3e28a5e7c04721fdc42c2d93ca1f65
Tyoma Gorbunov <bivaetctoskazat123@gmail.com>
2025-05-18 17:13:27 +0500

    feat: Добавлена команда init

--- master/cvs/commands/AbstractCommand.py
+++ master/cvs/commands/AbstractCommand.py
@@ -1,3 +1,4 @@
+import os
 from abc import ABC, abstractmethod
 
 
@@ -5,3 +6,9 @@ class AbstractCommand(ABC):
     @abstractmethod
     def run(self, args):
         pass
+
+    @classmethod
+    def _check_repository_initialized(cls):
+        if not os.path.isdir(os.path.join(os.getcwd(), ".cvs")):
+            print("\033[91mError: CVS repository not initialized in this directory.\033[0m")
+            exit(1)
--- master/cvs/commands/InitCommand.py
+++ master/cvs/commands/InitCommand.py
@@ -1,5 +1,9 @@
+import ctypes
+import os
+
 from .AbstractCommand import AbstractCommand
 from master.models.command import Init
+from ..service import HeadFileHandler
 
 
 class InitCommand(AbstractCommand):
@@ -8,4 +12,26 @@ class InitCommand(AbstractCommand):
         self.description = Init.description
 
     def run(self, args):
-        print("init run")
+        self.__init_folder()
+        self.__init_cvs_insides()
+
+    def __init_folder(self):
+        path = os.getcwd()
+        cvs_dir = os.path.join(path, ".cvs")
+        self.__cvs_dir = cvs_dir
+        if os.path.exists(cvs_dir):
+            print("\033[93mRepository is already initialized\033[0m")
+            exit(1)
+        os.makedirs(cvs_dir)
+        if os.name == 'nt':
+            attr_hidden = 0x02
+            ctypes.windll.kernel32.SetFileAttributesW(cvs_dir, attr_hidden)
+        print(f"\033[92mInitialized empty CVS repository in {path}\033[0m")
+
+    def __init_cvs_insides(self):
+        objects_dir = os.path.join(self.__cvs_dir, "objects")
+        refs_dir = os.path.join(self.__cvs_dir, "refs", "heads")
+        os.makedirs(objects_dir)
+        os.makedirs(refs_dir)
+        head_path = os.path.join(self.__cvs_dir, "HEAD")
+        HeadFileHandler(head_path).change_branch("master")
