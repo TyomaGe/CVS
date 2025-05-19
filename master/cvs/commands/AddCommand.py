76b4e59470fa9d9cab7ac2226adc4a2a2823d036
Tyoma Gorbunov <bivaetctoskazat123@gmail.com>
2025-05-19 11:55:48 +0500

    feat: Позволяет в add передать директорию, и все файлы из этой директории попадут в файл index

--- master/cvs/commands/AddCommand.py
+++ master/cvs/commands/AddCommand.py
@@ -16,17 +16,35 @@ class AddCommand(AbstractCommand):
         self.__dir, self.__cvs_dir = self._get_dirs_paths()
 
     def run(self, args):
+        self._check_repository_initialized()
         self.__files = args.files
         path_maker = PathMaker()
         index_handler = IndexFileHandler(self.__cvs_dir)
-        object_maker = CVSObjectsMaker(Blob.value, self.__cvs_dir)
+        obj_maker = CVSObjectsMaker(Blob.value, self.__cvs_dir)
+
         for file in self.__files:
-            abs_file_path = path_maker.make_path(self.__dir, file)
-            if not os.path.exists(abs_file_path):
-                print(f"\033[91mFile '{file}' does not exist\033[0m")
+            abs_path = path_maker.make_path(self.__dir, file)
+            if not os.path.exists(abs_path):
+                print(f"\033[91mPath '{file}' does not exist\033[0m")
                 continue
-            sha1 = object_maker.make_object(abs_file_path)
-            index_handler.add(file, sha1)
+            if os.path.isfile(abs_path):
+                self.__add_file(file, abs_path, obj_maker, index_handler)
+            else:
+                self.__add_directory(abs_path, obj_maker, index_handler,
+                                     path_maker)
+
+    @classmethod
+    def __add_file(cls, relative_path, abs_path, obj_maker, index_handler):
+        sha1 = obj_maker.make_object(abs_path)
+        index_handler.add(relative_path, sha1)
+
+    def __add_directory(self, abs_dir, obj_maker, index_handler, path_maker):
+        for root, _, files in os.walk(abs_dir):
+            for f in files:
+                abs_file_path = path_maker.make_path(root, f)
+                relative_file_path = os.path.relpath(abs_file_path, self.__dir)
+                self.__add_file(relative_file_path, abs_file_path,
+                                obj_maker, index_handler)
 
     @classmethod
     def get_args(cls, parser):
